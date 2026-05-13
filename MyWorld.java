efa0cdbe491f47d0577af14186a7874dd9993977
Super-JSB <lujessie099@gmail.com>
2026-05-13 15:52:20 -0400

    created elephant class

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,9 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
     }
 }
