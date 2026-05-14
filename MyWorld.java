3c9436f0e24b92093068eae3315ad4bc224ef6ff
Super-JSB <lujessie099@gmail.com>
2026-05-14 11:32:55 -0400

    add label

--- MyWorld.java
+++ MyWorld.java
@@ -1,15 +1,31 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
     public MyWorld() {
         super(600, 400, 1);
         
+        //Create the elephant object
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
+        
+        //Create a label
+        scoreLabel = new Label(0,80);
+        addObject(scoreLabel, 50, 50);
         
         createApple();
     }
     
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.
+    }
+
     /**
      * create a new apple at random location at the top of screen
      */
